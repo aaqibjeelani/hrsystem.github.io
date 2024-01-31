960bcf7ade1a08fe101302bbf09248d6b87ffe41
Aaqib jeelani <ajvirus1@gmail.com>
2024-02-01 00:13:28 +0530

    update

--- view/footer.php
+++
@@ -1,9 +0,0 @@
-	<footer class="main-footer">
-        <div class="pull-right hidden-xs">
-          <b>Version</b> 1.0.2
-        </div>
-        <strong>Copyright &copy; 2024-2027 <a href="http://aaqibjeelani.ddns.net">AJ virus</a>.</strong> All rights reserved.
-     </footer>
-      
-  </body>
-</html>
